efabf0ffe8738a7e822d6cf01bb876c451a519ea
Freshairkaboom <freshairkaboom@gmail.com>
2022-01-18 15:07:28 +0100

    First working unit tests
    Eureka! Finally got my unit tests to work. Only asserting the public interface and treating internals of component as a "black box".
    
    Two tests now passed as buttons that are generated by ingredientList containing true values. Checked for buttons being rendered as ingredientList containing both malts and hops.

--- tests/unit/Ingredients.spec.js
+++ tests/unit/Ingredients.spec.js
@@ -1,39 +1,118 @@
-import { mount } from '@vue/test-utils'
-import { createStore } from 'vuex'
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 import Ingredients from '@/components/Ingredients.vue'
 
 //Error: TypeError: Cannot read properties of undefined (reading 'state');
 
-describe('Ingredients.vue', () => {
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
+describe("Ingredients", () => {
     let wrapper;
     let store;
-    let global;
+    let beer;
+    let mockSelectFnc;
+    let state = {selectedBeer: jest.fn()}
 
-    store = createStore({
-        state() {
-            return {
-                selectedBeer: "",
+    mockSelectFnc = jest.fn();
+    store = new Vuex.Store({
+        state,
+        getters: {getSelectedBeer: ()=>{jest.fn()}},
+        mutations: {select: mockSelectFnc },
+    })
+
+    test('Renders button when ingredientList contains malts.', () => {
+
+    wrapper = mount(Ingredients, {
+        store,
+        localVue,
+        propsData: {
+          ingredientList: [
+            {
+            "name": "Maris Otter Extra Pale",
+            "amount": {
+            "value": 3.3,
+            "unit": "kilograms"
             }
-        },
-        getters: {
-            getSelectedBeer: state => {return state.selectedBeer},
-        },
-        mutations: {
-              setSelectedBeer(state, beer) {
-                state.selectedBeer = beer;
+            },
+            {
+            "name": "Caramalt",
+            "amount": {
+            "value": 0.2,
+            "unit": "kilograms"
             }
-        },
-        actions: {
-            mutateSelectedBeer({commit}, beer) {
-                   commit("setSelectedBeer", beer);
+            },
+            {
+            "name": "Munich",
+            "amount": {
+            "value": 0.4,
+            "unit": "kilograms"
             }
+            }
+            ]
         }
     })
 
-    it('Renders button.', () => {
+    var button = wrapper.get("button");
 
-    wrapper = mount(Ingredients, () => {
+    expect(button).toBeTruthy;
 
+  })
+    test('Renders button when ingredientList contains hops.', () => {
+
+    wrapper = mount(Ingredients, {
+        store,
+        localVue,
+        propsData: {
+          ingredientList: [
+            {
+            "name": "Fuggles",
+            "amount": {
+            "value": 25,
+            "unit": "grams"
+            },
+            "add": "start",
+            "attribute": "bitter"
+            },
+            {
+            "name": "First Gold",
+            "amount": {
+            "value": 25,
+            "unit": "grams"
+            },
+            "add": "start",
+            "attribute": "bitter"
+            },
+            {
+            "name": "Fuggles",
+            "amount": {
+            "value": 37.5,
+            "unit": "grams"
+            },
+            "add": "middle",
+            "attribute": "flavour"
+            },
+            {
+            "name": "First Gold",
+            "amount": {
+            "value": 37.5,
+            "unit": "grams"
+            },
+            "add": "middle",
+            "attribute": "flavour"
+            },
+            {
+            "name": "Cascade",
+            "amount": {
+            "value": 37.5,
+            "unit": "grams"
+            },
+            "add": "end",
+            "attribute": "flavour"
+            }
+            ]
+        }
     })
 
     var button = wrapper.get("button");
